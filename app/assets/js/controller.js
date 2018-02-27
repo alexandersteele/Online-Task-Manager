408b4ee866cf52f840c9c63dcecc9d7416caa0d1
Alexander Steele <sc16a2s@leeds.ac.uk>
2018-02-27 01:01:18 +0000

    Fixed http resource for find functionality

--- app/assets/js/controller.js
+++ app/assets/js/controller.js
@@ -1,4 +1,4 @@
-var myApp = angular.module("myApp", ['ngRoute', 'ngSanitize', 'mgcrea.ngStrap']);
+var myApp = angular.module("myApp", ['ngRoute', 'ngSanitize', 'ngResource', 'mgcrea.ngStrap']);
 
 myApp.config(function ($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
@@ -23,18 +23,20 @@ myApp.config(function ($routeProvider, $locationProvider) {
 
 })
 
-myApp.controller('indexCtrl', function ($scope, tasks, $alert, $http) {
-    $http.get('http://127.0.0.1:5000/tasks').then(function(result){
-       $scope.tasks = result.data;
-   }), function(){
-       window.alert('There was an error!');
-   }; 
-    //$scope.contacts = contacts.get();
+myApp.controller('indexCtrl', function ($scope, tasks, $alert) {
+    $scope.tasks = tasks.get();
 
 });
 
 myApp.controller('taskCtrl', function ($scope, $routeParams, tasks) {
-    $scope.task = tasks.find($routeParams.id);
+    var response = tasks.find($routeParams.id)
+    response.$promise.then(function onSuccess(data) {
+    // access data from 'response'
+      $scope.task = data[0];
+    },
+    function onFail(data) {
+    // handle failure
+    });
 });
 
 myApp.controller('addTaskCtrl', function ($scope, tasks) {
@@ -45,31 +47,21 @@ myApp.controller('addTaskCtrl', function ($scope, tasks) {
 }; 
 });
 
-myApp.factory('tasks', function () {
-    var tasks = [
-        {
-            owner: 'Alex',
-            title: 'Mow lawn',
-            task: 'Mow all of the local lawns',
-            completed: 'false'
-        },
-        {
-            owner: 'Alex',
-            title: 'Mow lawn',
-            task: 'Mow all of the local lawns',
-            completed: 'false'
+myApp.factory('tasks', function ($resource) {
+    
+    var resource = $resource('http://localhost:5000/tasks/:id', {id: '@id'}, {
+        update: {
+            method: 'PUT'
         }
-    ];
-
+    });
     return {
         get: function () {
-            return tasks;
+            return resource.query();
         },
-        find: function (index) {
-            return tasks[index];
-        },
-        create: function (task) {
-            tasks.push(task);
+        find: function(id){
+            return $resource('http://localhost:5000/task/:id').query({id: id});
         }
     };
+    
+    
 })
