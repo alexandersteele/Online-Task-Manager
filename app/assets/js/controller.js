53a1ab82fc8ba7bce1e574e3f3df28a455786919
Alexander Steele <sc16a2s@leeds.ac.uk>
2018-02-28 01:18:33 +0000

    Field editing added

--- app/assets/js/controller.js
+++ app/assets/js/controller.js
@@ -24,6 +24,7 @@ myApp.config(function ($routeProvider, $locationProvider) {
 })
 
 myApp.controller('indexCtrl', function ($scope, tasks, $alert) {
+
     $scope.tasks = tasks.get();
 
 });
@@ -31,25 +32,28 @@ myApp.controller('indexCtrl', function ($scope, tasks, $alert) {
 myApp.controller('taskCtrl', function ($scope, $routeParams, tasks) {
     var response = tasks.find($routeParams.id)
     response.$promise.then(function onSuccess(data) {
-    // access data from 'response'
-      $scope.task = data[0];
-    },
-    function onFail(data) {
-    // handle failure
-    });
+            // access data from 'response'
+            $scope.task = data[0];
+        },
+        function onFail(data) {
+            // handle failure
+        });
 });
 
 myApp.controller('addTaskCtrl', function ($scope, tasks) {
-    $scope.submit = function(){
-       tasks.create($scope.task);
-       $scope.task = null;
-       $scope.added = true;
-}; 
+    $scope.submit = function () {
+        $scope.task.$save();
+        $scope.task = tasks.create();
+    };
+
+    $scope.task = tasks.create();
 });
 
 myApp.factory('tasks', function ($resource) {
-    
-    var resource = $resource('http://localhost:5000/tasks/:id', {id: '@id'}, {
+
+    var resource = $resource('http://localhost:5000/tasks/:id', {
+        id: '@id'
+    }, {
         update: {
             method: 'PUT'
         }
@@ -58,10 +62,43 @@ myApp.factory('tasks', function ($resource) {
         get: function () {
             return resource.query();
         },
-        find: function(id){
-            return $resource('http://localhost:5000/task/:id').query({id: id});
+        find: function (id) {
+            return $resource('http://localhost:5000/task/:id').query({
+                id: id
+            });
+        },
+        create: function () {
+            return new resource();
         }
     };
-    
-    
+
+
+})
+
+myApp.directive('editable', function () {
+    return {
+        restrict: 'A',
+        templateUrl: '/partials/editable.html',
+        scope: {
+            value: '=editable',
+            field: '@fieldType'
+        },
+        controller: function ($scope) {
+            $scope.editor = {
+                showing: false,
+                value: $scope.value
+            };
+
+            $scope.toggleEditor = function () {
+                $scope.editor.showing = !$scope.editor.showing;
+            };
+
+            $scope.field = ($scope.field) ? $scope.field : 'text';
+
+            $scope.save = function () {
+                $scope.value = $scope.editor.value;
+                $scope.toggleEditor();
+            };
+        }
+    };
 })
